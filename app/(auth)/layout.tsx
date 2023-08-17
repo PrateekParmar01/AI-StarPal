aa1bd428caffcfb6a1e5396b227ca9a7b356a673
Prateek Parmar <prateekparmar2003@gmail.com>
2023-08-17 09:51:54 +0530

    First Commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+const layout = ({
+    children
+}:{
+    children: React.ReactNode;
+}) => {
+  return (
+    <div className="flex h-screen items-center justify-center">
+        {children}
+    </div>
+  )
+}
+
+export default layout
