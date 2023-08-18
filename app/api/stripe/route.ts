e01d41f2fc8bfe0ca0cb3ce51cbc134f9b1fe440
Prateek Parmar <prateekparmar2003@gmail.com>
2023-08-18 23:15:36 +0530

    fix 2

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -5,7 +5,8 @@ import prismadb from "@/lib/prismadb";
 import { stripe } from "@/lib/stripe";
 import { absoluteUrl } from "@/lib/utils";
 
-const settingsUrl = absoluteUrl("/settings");
+// const settingsUrl = absoluteUrl("/settings");
+const settingsUrl = "https://ai-star-pal-g56r.vercel.app/settings";
 
 export async function GET() {
   try {
